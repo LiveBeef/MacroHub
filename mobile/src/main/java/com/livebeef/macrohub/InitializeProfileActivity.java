9747d7709c0b4ac548f79c4e6c22c00fbe5efd86
aszoke2 <zoke2556@gmail.com>
2017-10-04 22:43:35 -0400

    Initial commit

---
+++ mobile/src/main/java/com/livebeef/macrohub/InitializeProfileActivity.java
@@ -0,0 +1,13 @@
+package com.livebeef.macrohub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InitializeProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initialize_profile);
+    }
+}
