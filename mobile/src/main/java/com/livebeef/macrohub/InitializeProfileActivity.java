b041f0111a9e1532adcb0e229e9e14df6733b4ef
aszoke2 <zoke2556@gmail.com>
2017-10-07 10:55:01 -0400

    Initialize Profile Activity

--- mobile/src/main/java/com/livebeef/macrohub/InitializeProfileActivity.java
+++ mobile/src/main/java/com/livebeef/macrohub/InitializeProfileActivity.java
@@ -1,13 +1,38 @@
 package com.livebeef.macrohub;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class InitializeProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_initialize_profile);
+
+        final Button initialManualButton = (Button) findViewById(R.id.initialManualButton);
+        initialManualButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent launchManualMacroInputIntent = new Intent(InitializeProfileActivity.this,
+                        ManualMacroInputActivity.class);
+                startActivity(launchManualMacroInputIntent);
+            }
+        });
+        final Button initialCalculateButton = (Button) findViewById(R.id.initialCalculateButton);
+        initialCalculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent launchCalculateMacroInputIntent = new Intent(InitializeProfileActivity.this,
+                        CalculateMacro1Activity.class);
+                startActivity(launchCalculateMacroInputIntent);
+            }
+        });
     }
+
 }
---
+++ mobile/src/main/java/com/livebeef/macrohub/ManualMacroInputActivity.java
@@ -0,0 +1,13 @@
+package com.livebeef.macrohub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManualMacroInputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manual_macro_input);
+    }
+}
